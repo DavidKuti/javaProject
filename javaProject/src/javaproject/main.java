22f0590e15303bfeca2de66a8f0807df56a72cdf
David Kuti <davidkuti2005@gmail.com>
2024-03-10 22:03:31 +0100

    Create main.java

---
+++ javaProject/src/javaproject/main.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaproject;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author david
+ */
+public class main extends JFrame {
+    
+}
